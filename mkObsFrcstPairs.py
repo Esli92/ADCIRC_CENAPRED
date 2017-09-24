063cc586597ca013614ed019e9d143079f88d1a9
Esli92 <moca92@gmail.com>
2017-09-23 23:20:42 -0500

    Hice algo de series creo? justo fue el 19s :(

--- mkObsFrcstPairs.py
+++ mkObsFrcstPairs.py
@@ -236,9 +236,10 @@ for value in pair_list:
 adcirc_series = pd.Series(adcirc_forecast, index=dates)
 observ_series = pd.Series(observation,index=dates)
 joint_series = pd.DataFrame({'obs' : observ_series,'adc' : adcirc_series})
-mean_series = pd.DataFrame({'obs-mean' : observ_series - observ_series.mean(),'adc-mean' : adcirc_series - adcirc_series.mean()})
+mean_series = pd.DataFrame({'obs-mean' : observ_series - observ_series.mean(),'adcirc-mean' : adcirc_series - adcirc_series.mean()})
 
 mean_series.plot()
 plt.ylabel('Elevacion (m)')
 figstr = 'ts_plot_d_{}_m_{}_node_{}.png'.format(wrf_day,wrf_month,adcirc_node)
 plt.savefig(figstr)
+
